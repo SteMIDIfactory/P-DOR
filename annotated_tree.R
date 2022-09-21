17d8dc5d6e46bbf9fcb07a0f8810ae605b703fd2
Gherard Batisti Biffignandi <99735635+gbatbiff@users.noreply.github.com>
2022-09-21 12:36:02 +0200

    Update annotated_tree.R

--- annotated_tree.R
+++ annotated_tree.R
@@ -1,5 +1,5 @@
 rm(list=ls())
-#setwd("/home/ghepard/PDOR/simul_EBI_retreat/")
+
 library(svglite)
 library(pheatmap)
 library(RColorBrewer)
@@ -15,7 +15,11 @@ cluster_file<-read.csv(list.files(pattern = "csv"),sep='')
 cluster_file$cluster<-as.character(cluster_file$cluster)
 cluster_file$STRAIN_ID<-as.character(cluster_file$STRAIN_ID)
 
-tree <- read.tree("RAxML_bipartitions.coreSNPs_Phylo.nwk")
+logfile<-read.delim("PDOR.log",header = F,sep='\t')
+ref_name <- logfile[which(logfile$V1=="ref"),"V2"]
+ref_name
+#tree <- read.tree("RAxML_bipartitionsBranchLabels.coreSNPs_Phylo.nwk")
+tree <- read.tree(snp_phylotree)
 
 add<-as.data.frame(tree$tip.label)
 colnames(add)<-"STRAIN_ID"
@@ -30,10 +34,15 @@ rr$STRAIN_ID<-NULL
 
 
 report<-read.delim("summary_resistance_virulence",sep='\t')
+
 report$X.COVERAGE<-as.character(report$X.COVERAGE)
 report$X.IDENTITY<-as.character(report$X.IDENTITY)
+new_ref_name<-paste(unique(report[which(report$X.FILE==ref_name),"SEQUENCE"]),".fna",sep="")
+
+report$X.FILE<-gsub(ref_name,new_ref_name,report$X.FILE)
 
 report<-report[grep("fna|fasta",report$X.FILE),]
+
 report$X.FILE<-gsub(".fna","",report$X.FILE)
 report$X.FILE<-gsub(".fasta","",report$X.FILE)
 
@@ -44,22 +53,33 @@ report_vir<-report[which(as.numeric(report$X.COVERAGE)>90 & as.numeric(report$X.
 report_vir<-report_vir[grep("ecoli_vf|vfdb",report_vir$DATABASE),]
 
 report_summary<-rbind(report_res,report_vir)
-
 tab<-table(report_summary$X.FILE,report_summary$GENE)
+# tab[tab==0]<-"absence"
+# tab[tab>=1]<-"presence"
 mtab<-as.matrix(tab)
 mtab[mtab>1]<-1
 colnames(mtab)<-gsub("Bla","",colnames(mtab))
 colnames(mtab)<-gsub("[(]","",colnames(mtab))
 colnames(mtab)<-gsub("[)]","",colnames(mtab))
 
+mtab1<-matrix(as.character(mtab),ncol = ncol(mtab))
+rownames(mtab1)<-rownames(mtab)
+colnames(mtab1)<-colnames(mtab)
+
 palette2 <- c("0" = "grey95",
               "1" = "#960018")
+
+
+
+cluster_file<-read.csv("clusters_manual_threshold_17_snps.csv",sep='')
+cluster_file$cluster<-as.character(cluster_file$cluster)
+cluster_file$STRAIN_ID<-as.character(cluster_file$STRAIN_ID)
+
 
 add<-as.data.frame(tree$tip.label)
 colnames(add)<-"STRAIN_ID"
 
 mm<-merge(cluster_file,add,by="STRAIN_ID",all = T)
-
 mm<-mm[which(mm$STRAIN_ID %in% tree$tip.label),]
 
 rr<-unique(rbind(mm,cluster_file))
@@ -75,22 +95,21 @@ names(color_vec) <- unique(rr$cluster)
 color_vec[names(color_vec) == "Other"] <- "darkgrey"
 
 
+
 pp <- p %<+% rr + geom_tiplab(aes(color=cluster,fontface=2),size=5,show.legend = FALSE)+
   geom_tippoint(aes(color=cluster),size=5,alpha = 0)+#scale_color_discrete(breaks = levels(factor(rr$cluster)))+
-  theme_tree2()+theme(legend.text=element_text(size=20),legend.title =element_text(size=20))+
+  theme_tree2()+theme(legend.text=element_text(size=20),legend.title = element_text(size=20))+
   guides(colour = guide_legend("Cluster", override.aes = list(size = 20, alpha = 1)))+
-  scale_color_manual(values = color_vec)
+  scale_color_manual(values = color_vec)#+geom_fruit(geom=geom_point,mapping=aes(color=cluster),size=3)
 
 
-#+geom_fruit(geom=geom_point,mapping=aes(color=cluster),size=3)
-
-
-hm <- gheatmap(pp,mtab, offset = 2, width=0.2, font.size=5, colnames_position= "top",
-         colnames_angle = 90, colnames_offset_y = 0, hjust = 0) +
+hm <- gheatmap(pp,mtab1, offset = 2, width=0.2, font.size=5, colnames_position= "top",
+         colnames_angle = 90, colnames_offset_y = -0.2, hjust = 0) +
          scale_fill_manual(values = c("0" = "grey95","1" = "#960018"))+
          scale_y_continuous(limits=c(-1, NA))+ guides(fill="none")
 
 
-                                                                  
-ggsave(hm, filename = "annotated_tree.svg",width = length(tree$tip.label)*0.7, height=length(tree$tip.label)*0.7 ,
-       units = "cm",scale=1.5)
+                                                                   
+ggsave(hm, filename = "annotated_tree.svg",width = length(tree$tip.label)*1.5, height=length(tree$tip.label)*2.5 ,
+        units = "cm",scale=2,limitsize = F)
+
