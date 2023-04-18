b4f9984901bb825bd150cb495591f228a950ba30
Gherard Batisti Biffignandi <99735635+gbatbiff@users.noreply.github.com>
2023-04-18 17:48:34 +0200

    Update contact_network.R

--- contact_network.R
+++ contact_network.R
@@ -1,14 +1,13 @@
 rm(list=ls())
 
 suppressPackageStartupMessages({
-
-library(ggnewscale)
-library(ggplot2)
-library(reshape2)
-library(svglite)
-library(dplyr)
-library(RColorBrewer)
-
+  
+  library(ggnewscale)
+  library(ggplot2)
+  library(svglite)
+  library(dplyr)
+  library(RColorBrewer)
+  
 })
 
 
@@ -20,8 +19,7 @@ hospitalization_file <- args[1]
 
 hospitalization<-read.delim(hospitalization_file,sep='\t')
 
-#hospitalization<-read.delim("sample_metadata_table.txt",sep='\t')
-
+#hospitalization<-read.delim("sample_metadata_table_2.txt",sep='\t')
 ###########################################################################
 hospitalization$STRAIN_ID<-gsub(".fna","",hospitalization$STRAIN_ID)
 hospitalization$STRAIN_ID<-gsub(".fasta","",hospitalization$STRAIN_ID)
@@ -50,8 +48,7 @@ cluster_file<-read.csv(list.files(pattern = "csv"),sep='')
 cluster_file$cluster<-as.character(cluster_file$cluster)
 cluster_file$STRAIN_ID<-as.character(cluster_file$STRAIN_ID)
 
-out<-grep("BD_",cluster_file$STRAIN_ID,value = T)
-
+out<-grep("BD",cluster_file$STRAIN_ID,value = T)
 cluster_file<-cluster_file[-which(cluster_file$STRAIN_ID %in% out),]
 
 freq_clust<-as.data.frame(table(cluster_file$cluster))
@@ -63,7 +60,9 @@ mm<-merge(hospitalization,cluster_file,by = "STRAIN_ID",all = T)
 clust<-select(mm,ISOLATION_DATE,PATIENT_ID, cluster)
 clust<-clust[which(!is.na(clust$cluster)),]
 moving<-unique(select(mm,PATIENT_ID,WARD_DATE_ENTRY, WARD_DATE_EXIT,WARD2))
+moving<-moving[which(!is.na(moving$PATIENT_ID)),]
 isol<-select(mm,ISOLATION_DATE,PATIENT_ID, status)
+isol<-isol[which(!is.na(isol$PATIENT_ID)),]
 ###
 
 
@@ -77,25 +76,26 @@ names(ward) <- levels(factor(moving$WARD2))
 #ward<-pal_ward[1:length(levels(factor(moving$WARD2)))]
 #names(ward) <- levels(factor(moving$WARD2))
 
-
 suppressWarnings(contact_network_plot<-ggplot(moving, start=WARD_DATE_ENTRY, end=WARD_DATE_EXIT, aes(WARD_DATE_ENTRY, PATIENT_ID)) +
-  theme_classic()+
-  geom_segment(data = moving,mapping = aes(color=WARD2, group=PATIENT_ID,
-                   y=PATIENT_ID,xend=WARD_DATE_EXIT,yend=PATIENT_ID),alpha = .7, size=10)+
-  scale_x_datetime(date_breaks='7 day',date_labels = "%Y-%m-%d")+
-  geom_point(data=isol, aes(ISOLATION_DATE,PATIENT_ID, shape=status),
-             #position=position_jitter(h=0.02),
-             alpha=1,size=5,stroke = 0.5)+scale_shape_discrete(solid=F)+
-  labs(x = NULL, y = NULL, color = "ward")+scale_color_manual(values = ward)+
-  theme(axis.text.x=element_text(angle = 45, hjust = 1,size=15),
-        axis.text.y=element_text(hjust = 1,size=15),
-        legend.text=element_text(size=15),
-        legend.title =element_text(size=20),
-        axis.title.x = element_text(hjust = 1,size=15))+
-  labs(shape="condition")+ scale_linetype(guide = "none") +ggnewscale::new_scale_color() +
-  geom_line(data=clust, aes(ISOLATION_DATE,PATIENT_ID, group=cluster, linetype=cluster,color=cluster),size=1.2,alpha=1)+
-  scale_color_manual(values = brewer.pal(length(levels(factor(clust$cluster))), "Set2"))
-
+                   theme_classic()+
+                   geom_segment(data = moving,mapping = aes(color=WARD2, group=PATIENT_ID,
+                   y=PATIENT_ID,xend=WARD_DATE_EXIT,yend=PATIENT_ID),alpha = .7, size=length(moving$PATIENT_ID)*0.7)+
+                   scale_x_datetime(date_breaks='7 day',date_labels = "%Y-%m-%d")+
+                   geom_point(data=isol, aes(ISOLATION_DATE,PATIENT_ID, shape=status),
+                              #position=position_jitter(h=0.02),
+                              alpha=1,size=length(moving$PATIENT_ID)*0.4,stroke = 0.5)+scale_shape_discrete(solid=F)+
+                   labs(x = NULL, y = NULL, color = "ward")+scale_color_manual(values = ward)+
+                   theme(axis.text.x=element_text(angle = 45, hjust = 1,size=length(moving$WARD_DATE_ENTRY)*0.9),
+                         axis.text.y=element_text(hjust = 1,size=length(moving$PATIENT_ID)*0.9),
+                         legend.text=element_text(size=length(moving$PATIENT_ID)*0.8),
+                         legend.title =element_text(size=length(moving$PATIENT_ID)*1.2),
+                         axis.title.x = element_text(hjust = 1,size=length(moving$PATIENT_ID)*0.7))+
+                   labs(shape="condition")+ scale_linetype(guide = "none") +ggnewscale::new_scale_color() +
+                   geom_line(data=clust, aes(ISOLATION_DATE,PATIENT_ID, group=cluster, linetype=cluster,color=cluster),
+                             size=length(moving$PATIENT_ID)*0.08,alpha=1)+
+                   scale_color_manual(values = brewer.pal(length(levels(factor(clust$cluster))), "Set2"))
+                   
+                  
 )
 
 
@@ -105,5 +105,4 @@ var_height<-length(ggplot_build(contact_network_plot)$layout$panel_params[[1]]$y
 var_width<-length(ggplot_build(contact_network_plot)$layout$panel_params[[1]]$x$get_labels())
 
 ggsave(plot = contact_network_plot, filename = "contact_network_plot.svg",
-       width = var_width*1.2, height=var_height*1.2 , units = "cm",limitsize = F,scale = 1)
-
+       width = var_width*3, height=var_height*3 , units = "cm",limitsize = F,scale = 1)
